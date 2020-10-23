219a618b46a807f44d449ff3fb999c753b6dbb77
Muhammad Isfaaq Goomany <isfaaqg@gmail.com>
2020-10-23 15:04:34 +0400

    log out

--- lib/views/profile.screen.dart
+++ lib/views/profile.screen.dart
@@ -1,44 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:werecycle/main.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text("Profile"),
+        actions: [
+          IconButton(
+            icon: Icon(Feather.log_out),
+            onPressed: () {
+              GetStorage().erase();
+              runApp(MyApp());
+            },
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              constraints: const BoxConstraints(maxHeight: 340),
+              margin: const EdgeInsets.symmetric(horizontal: 32),
+              child: Image.asset("assets/images/profile-image.png"),
+            ),
+          ),
+          Expanded(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text("Profile"),
-                ),
-                Container(
-                  constraints: const BoxConstraints(maxHeight: 340),
-                  margin: const EdgeInsets.symmetric(horizontal: 32),
-                  child: Image.asset("assets/images/profile-image.png"),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Feather.phone),
+                    title: Text("Phone Number"),
+                    subtitle: Text(GetStorage().read("phoneNumber")),
+                  ),
                 ),
               ],
             ),
-          ),
-        ),
-        Expanded(
-          child: Column(
-            children: [
-              Card(
-                child: ListTile(
-                  leading: Icon(Feather.phone),
-                  title: Text("Phone Number"),
-                  subtitle: Text(GetStorage().read("phoneNumber")),
-                ),
-              ),
-            ],
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
